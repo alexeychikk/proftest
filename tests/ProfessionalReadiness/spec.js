25f80e5214cc4870d648e9c34df5f607bcaa09b1
Сергей <vintorezvs@gmail.com>
2016-03-12 20:28:31 +0200

    Professional readiness + Psyhogeometrical + ThinkingAndCreativity

---
+++ tests/ProfessionalReadiness/spec.js
@@ -0,0 +1,20 @@
+let functionInput = [
+    {
+        skill: 1,
+        attitude: 2,
+        desire: 0
+    }
+    //...
+];
+
+let functionResult = {
+    collSums: [ // sums by columns
+        {
+            skill: 10,
+            attitude: 20,
+            desire: 25
+        }
+        //... length = 5
+    ],
+    likes: [0, 2 , 4, 3, 1] //indexes of likes, sorted by priority
+};
---
+++ tests/ThinkingAndCreativity/spec.js
@@ -0,0 +1,6 @@
+let functionInput = [true, false /*... length = 75 */];
+
+let functionResult = { // value = level in levels [low, medium, high]
+    objective: 'high', // key = key of thinking type
+    creativity: 'low' // key = 'creativity' always
+};
