5d78c02a85c1c108dc99087a11f3f444bd92f951
Сергей <vintorezvs@gmail.com>
2016-03-13 19:14:28 +0200

    ProfessionalReadiness func implemented

--- tests/ProfessionalReadiness/spec.js
+++ tests/ProfessionalReadiness/spec.js
@@ -8,7 +8,7 @@ let functionInput = [
 ];
 
 let functionResult = {
-    collSums: [ // sums by columns
+    columnsSums: [ // sums by columns
         {
             skill: 10,
             attitude: 20,
@@ -16,5 +16,58 @@ let functionResult = {
         }
         //... length = 5
     ],
-    likes: [0, 2 , 4, 3, 1] //indexes of likes, sorted by priority
+    likes: [0, 2, 4, 3, 1] //indexes of likes, sorted by priority
 };
+
+//let answers = [
+//    {skill: 1, attitude: 0, desire: 0},
+//    {skill: 1, attitude: 2, desire: 1},
+//    {skill: 0, attitude: 0, desire: 0},
+//    {skill: 0, attitude: 1, desire: 0},
+//    {skill: 1, attitude: 1, desire: 0},
+//    {skill: 1, attitude: 1, desire: 0},
+//    {skill: 1, attitude: 1, desire: 1},
+//    {skill: 0, attitude: 0, desire: 0},
+//    {skill: 0, attitude: 1, desire: 0},
+//    {skill: 1, attitude: 1, desire: 1},
+//    {skill: 0, attitude: 0, desire: 0},
+//    {skill: 2, attitude: 2, desire: 1},
+//    {skill: 0, attitude: 0, desire: 0},
+//    {skill: 0, attitude: 1, desire: 1},
+//    {skill: 1, attitude: 1, desire: 2},
+//    {skill: 0, attitude: 0, desire: 0},
+//    {skill: 2, attitude: 2, desire: 2},
+//    {skill: 0, attitude: 1, desire: 0},
+//    {skill: 0, attitude: 1, desire: 0},
+//    {skill: 2, attitude: 1, desire: 1},
+//    {skill: 1, attitude: 1, desire: 1},
+//    {skill: 1, attitude: 2, desire: 1},
+//    {skill: 0, attitude: 1, desire: 1},
+//    {skill: 0, attitude: 0, desire: 0},
+//    {skill: 2, attitude: 2, desire: 1},
+//    {skill: 2, attitude: 2, desire: 2},
+//    {skill: 2, attitude: 2, desire: 1},
+//    {skill: 0, attitude: 0, desire: 0},
+//    {skill: 0, attitude: 0, desire: 0},
+//    {skill: 1, attitude: 1, desire: 1},
+//    {skill: 1, attitude: 2, desire: 1},
+//    {skill: 1, attitude: 2, desire: 1},
+//    {skill: 0, attitude: 0, desire: 0},
+//    {skill: 1, attitude: 1, desire: 1},
+//    {skill: 1, attitude: 2, desire: 1},
+//    {skill: 2, attitude: 1, desire: 1},
+//    {skill: 1, attitude: 2, desire: 1},
+//    {skill: 1, attitude: 1, desire: 1},
+//    {skill: 0, attitude: 1, desire: 1},
+//    {skill: 1, attitude: 1, desire: 0},
+//    {skill: 2, attitude: 2, desire: 2},
+//    {skill: 1, attitude: 1, desire: 1},
+//    {skill: 0, attitude: 1, desire: 0},
+//    {skill: 0, attitude: 1, desire: 0},
+//    {skill: 1, attitude: 1, desire: 0},
+//    {skill: 1, attitude: 0, desire: 0},
+//    {skill: 2, attitude: 2, desire: 2},
+//    {skill: 0, attitude: 0, desire: 0},
+//    {skill: 0, attitude: 1, desire: 1},
+//    {skill: 1, attitude: 1, desire: 0}
+//];
