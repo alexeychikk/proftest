6837a92f07d7e14403acc3197fdebf07db7afce0
Зинкевич Алексей <alexzinkevych@yandex.ua>
2016-03-27 17:54:52 +0300

    icon-button directive

---
+++ client/components/icon-button/icon-button.directive.js
@@ -0,0 +1,15 @@
+"use strict";
+
+(function () {
+
+	angular.module('proftestApp')
+		.directive('iconButton', () => ({
+			templateUrl: 'components/icon-button/icon-button.html',
+			restrict: 'E',
+			scope: {
+				icon: '@',
+				tooltip: '@'
+			}
+		}));
+
+})();
