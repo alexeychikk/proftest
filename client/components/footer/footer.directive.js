a2af49182b4acb3bb100dcc84d4342e7a8d755da
Зинкевич Алексей <alexzinkevych@yandex.ua>
2016-04-02 15:49:37 +0300

    Footer improvement

--- client/components/footer/footer.directive.js
+++ client/components/footer/footer.directive.js
@@ -4,17 +4,27 @@
 
     angular.module('proftestApp')
         .directive('myFooter', () => {
-
             return {
                 templateUrl: 'components/footer/footer.html',
                 restrict: 'E',
                 replace: true,
                 link: (scope, element) => {
-                    element.addClass('footer');
+                    element.addClass('footer hidden');
                     scope.date = new Date();
+					var show = false;
+
+					function setShow() {
+						show = document.body.offsetHeight + document.body.scrollTop + 36 >= document.body.scrollHeight;
+						if (scope.show != show)
+							show ? element.removeClass('hidden') : element.addClass('hidden');
+						scope.show = show;
+					}
+
+					setInterval(function() {
+						setShow();
+					}, 35);
                 }
             };
-
         });
 
 })();
